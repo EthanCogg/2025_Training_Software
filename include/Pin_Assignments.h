a5c98185d131b4386ba887b81de1d655262885af
EthanCogg <eec229@umsystem.edu>
2024-11-19 19:11:40 -0600

    .

---
+++ include/Pin_Assignments.h
@@ -0,0 +1,9 @@
+
+#define PIN_ASSIGNMENTS_H
+
+#define SW_1 13 // toggle
+#define SW_2 15 // send current temperature
+// Temperature
+#define TEMP 21
+#define LED 5
+bool ledison = false;
--- src/TrainingSoftware.cpp
+++ src/TrainingSoftware.cpp
@@ -1,9 +1,28 @@
+#include "Pin_Assignments.h"
 #include <Arduino.h>
 
 void setup() {
+    Serial.begin(115200);
+    delay(10);
+    pinMode(SW_1, INPUT_PULLDOWN);
+    pinMode(SW_2, INPUT_PULLUP);
+    pinMode(LED, OUTPUT);
+
+    Serial.println("Initializing RoveComm");
+    RoveComm.begin
     // put your setup code here, to run once:
 }
 
 void loop() {
+    if (digitalRead(SW_1)) {
+        if (ledison) {
+            ledison = false;
+            digitalWrite(LED, LOW);
+        } else {
+            ledison = true;
+            digitalWrite(LED, HIGH);
+        }
+        delay(1000);
+    }
     // put your main code here, to run repeatedly:
 }
